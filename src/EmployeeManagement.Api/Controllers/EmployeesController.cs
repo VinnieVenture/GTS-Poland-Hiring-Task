61aeb03b0e4e1659fd7324f5dbb3834f1fa0f504
VinnieVenture <vinventurewow@gmail.com>
2026-09-17 16:30:01 +0200

    added controller

--- src/EmployeeManagement.Api/Controllers/EmployeesController.cs
+++ src/EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -1,43 +1,54 @@
+using EmployeeManagement.Api.Dtos;
+using EmployeeManagement.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Api.Controllers;
 
 [ApiController]
-public class EmployeesController : ControllerBase
+public class EmployeesController(IEmployeeService service) : ControllerBase
 {
     [HttpPost("employee")]
-    public IActionResult Create()
+    public async Task<IActionResult> Create(EmployeeRequestDto request, CancellationToken cancelToken)
     {
-        return Ok("TODO: create");
+        var result = await service.CreateAsync(request, cancelToken);
+        return result.IsSuccess
+            ? CreatedAtAction(nameof(GetById), new { id = result.Value!.Id }, result.Value)
+            : ToErrorResponse(result);
     }
 
-    [HttpGet("employee/{id}")]
-    public IActionResult GetById(Guid id)
+    [HttpGet("employee/{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancelToken)
     {
-        return Ok("TODO: get by id");
+        var result = await service.GetByIdAsync(id, cancelToken);
+        return result.IsSuccess ? Ok(result.Value) : ToErrorResponse(result);
     }
 
     [HttpGet("employees")]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll(CancellationToken cancelToken) =>
+        Ok(await service.GetAllAsync(cancelToken));
+
+    [HttpPut("employee/{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, EmployeeRequestDto request, CancellationToken cancelToken)
     {
-        return Ok("TODO: get all");
+        var result = await service.UpdateAsync(id, request, cancelToken);
+        return result.IsSuccess ? Ok(result.Value) : ToErrorResponse(result);
     }
 
-    [HttpPut("employee/{id}")]
-    public IActionResult Update(Guid id)
+    [HttpDelete("employee/{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancelToken)
     {
-        return Ok("TODO: update");
-    }
-
-    [HttpDelete("employee/{id}")]
-    public IActionResult Delete(Guid id)
-    {
-        return Ok("TODO: delete");
+        var result = await service.DeleteAsync(id, cancelToken);
+        return result.IsSuccess ? NoContent() : ToErrorResponse(result);
     }
 
     [HttpPost("employees/bulk")]
-    public IActionResult BulkImport()
+    public IActionResult BulkImport() => Ok("TODO: bulk import");
+
+    private IActionResult ToErrorResponse<T>(ServiceResult<T> result) => result.Error switch
     {
-        return Ok("TODO: bulk import");
-    }
+        ServiceError.Validation => ValidationProblem(new ValidationProblemDetails(result.ValidationErrors!)),
+        ServiceError.NotFound => Problem(statusCode: StatusCodes.Status404NotFound, title: "Employee not found."),
+        ServiceError.Conflict => Problem(statusCode: StatusCodes.Status409Conflict, title: "An employee with this email already exists."),
+        _ => throw new InvalidOperationException($"Unhandled service error: {result.Error}")
+    };
 }
