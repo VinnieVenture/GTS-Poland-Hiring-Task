eb556aa066a723ee5b49b1da75a40dbaee371440
VinnieVenture <vinventurewow@gmail.com>
2026-09-17 13:46:21 +0200

    added sscoped services with keys

--- src/EmployeeManagement.Api/Program.cs
+++ src/EmployeeManagement.Api/Program.cs
@@ -1,10 +1,15 @@
 using EmployeeManagement.Api.Data;
+using EmployeeManagement.Api.Dtos;
+using EmployeeManagement.Api.Validators;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,6 +17,12 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString, npgsqlOptions =>
         npgsqlOptions.EnableRetryOnFailure()));
+
+builder.Services.AddSingleton(TimeProvider.System);
+// Program.cs
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddKeyedScoped<IValidator<EmployeeRequestDto>, EmployeeRequestValidator>(ValidatorKeys.Create);
+builder.Services.AddKeyedScoped<IValidator<EmployeeRequestDto>, UpdateEmployeeRequestValidator>(ValidatorKeys.Update);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/EmployeeManagement.Api/Validators/ValidatorKeys.cs
@@ -0,0 +1,7 @@
+namespace EmployeeManagement.Api.Validators;
+
+public static class ValidatorKeys
+{
+    public const string Create = "employee-create";
+    public const string Update = "employee-update";
+}
